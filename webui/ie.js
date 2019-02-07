a8afc7969db8bccb9636b7ad729619df92ca4929
ShiYunJin <luoyibin_001@163.com>
2019-02-08 03:39:20 +0800

    feat: fix torrent title has tab json error

---
+++ webui/ie.js
@@ -0,0 +1,1 @@
+(function(){var a=(document.documentMode!=="undefined"&&document.documentMode)||6;if(a<=8){document.write('<style type="text/css">@import "./ie.css";</style>')}if(a<=7){document.write('<style type="text/css">@import "./ie7.css";</style>')}})();
